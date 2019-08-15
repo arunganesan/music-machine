aa6adf4462cfd4c107293a720ef5e2d46e702664
Arun Ganesan <arunganesan123@gmail.com>
2019-08-15 10:44:35 -0400

    .

--- tabla-database/classes.py
+++ tabla-database/classes.py
@@ -1,4 +1,139 @@
 class Composition ():
+    def __init__ (self, notes):
+        self.notes = notes
     def generate (self):
         return "Notes!"
 
+# 5 themes.jpg
+GRNG = Composition("Dha TT GRNG GRNG TRKT, Dha TT, GRNG TuNa KTTK")
+
+# not all things have theme/kali
+# some are tihais
+# some are tukras or chakradars - one off things
+# we need to specify the number of notes per bar/beat
+# tripadi has 3 notes per beat
+# and number of bars per cycle 
+#   -- rupak compositions have 7 bar -- 14 total per cycle
+#   -- teentaal ones have 16 per cycle, etc
+
+
+# bhajan beats 1.jpg
+# bhajan beats 2.jpg
+# bhajan beats.jpg
+# deri kaida.jpeg
+# dha dha dha dha chakradar.jpg
+# dha dha dha dha dhin dhin dhin dhin.jpeg
+# dha dha dha ka na ka dhi tihai.jpeg
+# dha dha dha kttk dhi 2.jpg
+# dha dha dha kttk dhi.jpg
+# dha dha dhin dhin tetetete.jpg
+# dha dha te te dha dha tu na.jpeg
+# dha dha trkt dha dha tu na more variations.jpg
+# dha dha trkt dha dha tu na p1.jpg
+# dha dha trkt dha dha tu na p2.jpg
+# dha dha trkt dha dha tu na p3.jpg
+# dha dha trkt dha dha tu na p4.jpg
+# dha dha trkt dha dha tu na.jpg
+# dha ge na dha trkt.jpeg
+# dha ge tt ge ge tt p1.jpg
+# dha ge tt ge ge tt p2.jpg
+# dha te te dha te te dha dha te te variations 1.jpeg
+# dha te te dha te te dha dha te te variations 2.jpg
+# dha te te dha te te dha dha te te variations 3.jpg
+# dha te te dha te te dha dha te te.jpg
+# dha te te dha te te dha na.jpg
+# dha tet dha dha.jpg
+# dha tet dha ge na dha trkt rupak p1.jpeg
+# dha tet dha ge na dha trkt rupak p2.jpeg
+# dha tet dha ge na dha trkt rupak p3.jpeg
+# dha tet dha ge na dha trkt.jpeg
+# dha ti dha ge na dha trkt peshkar.jpg
+# dha ti dha ge na dha trkt variations.jpg
+# dha ti dha ge na dha trkt.jpg
+# dha trkttk dhe te dhe te.jpg
+# dha trkttk drdr kttk.jpeg
+# dha trkttk drdr kttk.jpg
+# dha trkttk tak na na na na tak 2.jpg
+# dha trkttk tak na na na na tak.jpg
+# dha tt dha ge na.jpeg
+# dha tt dha tt dha dha tt.jpeg
+# dha tt grng variations 2.jpg
+# dha tt grng variations.jpg
+# dha tt grng.jpg
+# dha tu na dha.jpeg
+# dhatr ktgr drdrdr more variations.png
+# dhatr ktgr drdrdr.jpeg
+# dhe ne ge ne tak ka different tihai.jpg
+# dhe ne ge ne tak ka more variations.jpg
+# dhe ne ge ne tak ka.jpg
+# dhin dhin na trkt dhin ge na.jpg
+# dhin dhin na trkt dhin na.jpg
+# dhin dhin tete tukra.jpg
+# din din te te tukra.jpeg
+# drdr kttk tkt dha tukra 2.jpeg
+# drdr kttk tkt dha tukra.jpeg
+# drdr kttk tkt dha tukras.jpg
+# ge ge practice.jpg
+# ge ge te te ge ge na ghe.jpg
+# ge ge te te ke ke te te.jpg
+# ge ge te te ta ge te te.jpg
+# ge ge te te.jpeg
+# ge na dha tet p1.jpeg
+# ge na dha tet p2.jpeg
+# ge na na kat na na.jpeg
+# ge na tt ge na dha ge p1.jpg
+# ge na tt ge na dha ge p2.jpg
+# ge na tt ge na dha ge p3.jpg
+# gopatch tihai.jpeg
+# just variations.jpeg
+# just variations.jpg
+# kat ta ge ge tt kat ta - maybe same.jpeg
+# kat ta ge ge tt kat ta variations.jpg
+# kat ta ge ge tt kat ta.png
+# kat ta kat ta ge ge te te.jpeg
+# kat te dhe te te 2.jpg
+# kat te dhe te te.jpeg
+# kat te dhe te te.jpg
+# kat tet dhe te te.jpg
+# kat tt ge ge tt chakradhar.jpg
+# kat tt ge ge tt ga dhi tukra 2.jpeg
+# kat tt ge ge tt ga dhi tukra.jpeg
+# kat tt ge ge tt kaida.jpg
+# kat tt ge ge tt.jpeg
+# kat tt kat tt kat ta tukra.jpg
+# keherewa composition p1.jpg
+# keherewa composition p2.jpg
+# keherewa composition p3.jpg
+# keherewa composition p4.jpg
+# keherewa tukra.jpg
+# keherewa.jpeg
+# ktt ktt kt kt kt kt tukras.jpg
+# kttk practice.jpg
+# kttk trkt tktk trkt.jpg
+# mere dholna.jpeg
+# na ga dhe ne variations.jpeg
+# na ge dhe ne.jpg
+# one handed tukra.jpg
+# recital lots of stuff.jpeg
+# recital.jpeg
+# rela practice.jpg
+# rhythm practice.jpg
+# rupak.jpg
+# tak ka dhi tukra.jpg
+# tak ka di 2.jpg
+# tak ka di 3.jpg
+# tak ka di tukra.jpg
+# tihai practice 1.jpeg
+# tihais 1.jpg
+# tihais 2.jpg
+# tihais 3.jpg
+# tihais 4.jpg
+# tihais 5.jpg
+# tihais from 13.jpg
+# tin tal.jpg
+# tripadi variations.jpeg
+# trkt kaida.jpeg
+# trkt practice 1.jpeg
+# trkt practice 2.jpeg
+# trkt tktk practice 2.jpg
+# trkt tktk practice.jpg
